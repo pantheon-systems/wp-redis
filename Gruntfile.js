d23e2dbb736e91ef16ba7234e1824bf75c50c831
Daniel Bachhuber <daniel@handbuilt.co>
2015-11-12 14:04:50 -0800

    Compile README.md from readme.txt

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function( grunt ) {
+
+	'use strict';
+	// Project configuration
+	grunt.initConfig( {
+
+		pkg:    grunt.file.readJSON( 'package.json' ),
+
+		wp_readme_to_markdown: {
+			options: {
+				screenshot_url: 'https://s.w.org/plugins/{plugin}/{screenshot}.png',
+			},
+			your_target: {
+				files: {
+					'README.md': 'readme.txt'
+				}
+			},
+		},
+
+		phpcs: {
+			plugin: {
+				src: './'
+			},
+			options: {
+				bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
+				standard: "phpcs.ruleset.xml"
+			}
+		},
+
+	} );
+
+	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
+	grunt.loadNpmTasks( 'grunt-phpcs' );
+	grunt.registerTask( 'readme', ['wp_readme_to_markdown']);
+
+	grunt.util.linefeed = '\n';
+
+};
