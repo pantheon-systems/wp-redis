6da2760b86c310dd167362b0d9faf459145f1311
Daniel Bachhuber <daniel@handbuilt.co>
2016-07-01 07:54:08 -0700

    Add a comment explaining the file

--- bin/fixtures/redis-debug.php
+++ bin/fixtures/redis-debug.php
@@ -1,5 +1,12 @@
 <?php
 
+#####################
+# This plugin is placed in mu-plugins by the testing scripts. Once in mu-plugins
+# it will be loaded on all requests to WordPress and add Redis debug information
+# to to the footer of the site when a GET param is present. Behat can then
+# examine this debug output.
+#####################
+
 add_action( 'wp_footer', function(){
 	if ( isset( $_GET['redis_debug'] ) ) {
 		$GLOBALS['wp_object_cache']->stats();
