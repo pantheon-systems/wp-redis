345f70a8802fa6f4eac0e376ed31f9fa1e4f4b5f
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-17 11:34:17 -0700

    These environment variables are also required

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -11,12 +11,17 @@ if [ -z "$TERMINUS_TOKEN" ]; then
 	exit 0
 fi
 
-set -ex
-
 if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
 	echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
 	exit 1
 fi
+
+if [ -z "$WORDPRESS_ADMIN_USERNAME" ] || [ -z "$WORDPRESS_ADMIN_PASSWORD" ]; then
+	echo "WORDPRESS_ADMIN_USERNAME and WORDPRESS_ADMIN_PASSWORD environment variables must be set"
+	exit 1
+fi
+
+set -ex
 
 ###
 # Create a new environment for this particular test run.
