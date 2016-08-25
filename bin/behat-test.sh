616eb8f2e5bd1b8038d97781f5a09b9b8ca680c2
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-25 09:35:49 -0700

    Restore `TERMINUS_*` env checks

--- bin/behat-test.sh
+++ bin/behat-test.sh
@@ -10,6 +10,11 @@ if [ $? -ne 0 ]; then
 	exit 0
 fi
 
+if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
+	echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
+	exit 1
+fi
+
 if [ -z "$WORDPRESS_ADMIN_USERNAME" ] || [ -z "$WORDPRESS_ADMIN_PASSWORD" ]; then
 	echo "WORDPRESS_ADMIN_USERNAME and WORDPRESS_ADMIN_PASSWORD environment variables must be set"
 	exit 1
