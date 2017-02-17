21963e4a3d368d1ee0595a6f1ed5d08e06303a8a
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-17 07:56:12 -0800

    Catch warnings triggered by phpRedis in the `->connect()` method
    phpRedis seems to have begun triggering new warnings in 3.1.x

--- object-cache.php
+++ object-cache.php
@@ -1016,7 +1016,13 @@ class WP_Object_Cache {
 		} else { //tcp connection
 			$port = ! empty( $redis_server['port'] ) ? $redis_server['port'] : 6379;
 		}
-		$this->redis->connect( $redis_server['host'], $port, 1, null, 100 ); # 1s timeout, 100ms delay between reconnections
+		try {
+			$this->redis->connect( $redis_server['host'], $port, 1, null, 100 ); # 1s timeout, 100ms delay between reconnections
+		} catch ( PHPUnit_Framework_Error_Warning $e ) {
+			// PHPUnit throws an Exception when `trigger_error()` is called.
+			// To ensure our tests (which expect Exceptions to be caught) continue to run,
+			// we catch the PHPUnit exception and let code execution continue
+		}
 		foreach ( array( 'auth' => 'auth', 'database' => 'select' ) as $k => $method ) {
 			if ( ! isset( $redis_server[ $k ] ) ) {
 				continue;
