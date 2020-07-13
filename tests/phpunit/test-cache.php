83d8a43f8e834dfae2c8749aa7b746ff2ce300ce
Daniel Bachhuber <daniel@handbuilt.co>
2020-07-13 09:44:36 -0700

    Test seems to be passing now in PHP 7.4

--- tests/phpunit/test-cache.php
+++ tests/phpunit/test-cache.php
@@ -1336,9 +1336,6 @@ class CacheTest extends WP_UnitTestCase {
 		if ( ! class_exists( 'Redis' ) ) {
 			$this->markTestSkipped( 'PHPRedis extension not available.' );
 		}
-		if ( version_compare( PHP_VERSION, '7.4-alpha' ) >= 0 ) {
-			$this->markTestSkipped( 'Test fails unexpectedly in PHP 7.4' );
-		}
 
 		$redis = $this->getMockBuilder( 'Redis' )->getMock();
 		$redis->method( 'select' )
