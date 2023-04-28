333aa3d39601e98619a35c9c7cf873d1a3e718aa
Chris Reynolds <chris@jazzsequence.com>
2023-04-28 09:10:56 -0600

    wordpress isn't actually getting installed break the brackets

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -84,10 +84,8 @@ terminus build:workflow:wait $TERMINUS_SITE.$TERMINUS_ENV
 # Set up WordPress, theme, and plugins for the test run
 ###
 # Silence output so as not to show the password.
-{
-  echo "Installing WordPress..."
-  terminus wp $SITE_ENV -- core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=$WORDPRESS_ADMIN_USERNAME --admin_email=wp-redis@getpantheon.com --admin_password=$WORDPRESS_ADMIN_PASSWORD
-} &> /dev/null
+echo "Installing WordPress..."
+  terminus wp $SITE_ENV -- core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=$WORDPRESS_ADMIN_USERNAME --admin_email=wp-redis@getpantheon.com --admin_password=$WORDPRESS_ADMIN_PASSWORD &> /dev/null
 echo "Flush cache and setup environment..."
 terminus wp $SITE_ENV -- cache flush
 terminus wp $SITE_ENV -- plugin activate wp-redis classic-editor
