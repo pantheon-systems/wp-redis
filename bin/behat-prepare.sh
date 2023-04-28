c8fe6c8cd205060993e166993335060f2cdf0b42
Chris Reynolds <chris@jazzsequence.com>
2023-04-28 10:58:32 -0600

    enable redis early so we aren't potentially waiting for other things later

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -30,6 +30,10 @@ set -ex
 terminus env:create  $TERMINUS_SITE.dev $TERMINUS_ENV
 terminus env:wipe $SITE_ENV --yes
 
+# Enable Redis
+terminus redis:enable $TERMINUS_SITE
+terminus build:workflow:wait $SITE_ENV
+
 ###
 # Get all necessary environment details.
 ###
@@ -88,9 +92,7 @@ echo "Installing WordPress..."
 {
 	terminus wp $SITE_ENV -- core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=$WORDPRESS_ADMIN_USERNAME --admin_email=wp-redis@getpantheon.com --admin_password=$WORDPRESS_ADMIN_PASSWORD
 } &> /dev/null
-# Enable Redis
-terminus redis:enable $TERMINUS_SITE
-terminus build:workflow:wait $SITE_ENV
+
 terminus wp $SITE_ENV -- plugin activate wp-redis
 terminus wp $SITE_ENV -- redis enable
 
