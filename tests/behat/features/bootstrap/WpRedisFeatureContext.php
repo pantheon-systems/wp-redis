724845783cc3370ff55ef1fd81b07118068cb8e2
J Ryan Wagner <jryanwagner00@gmail.com>
2023-06-23 10:58:03 -0400

    [CMSP-459] Updates behat tests and behaviors. (#430)
    * Updates behat tests and behaviors.
    
    * Updates behat tests and behaviors.
    
    * port diff from #426
    
    * tabs to spaces
    
    * don't enable redis as part of the prepare step
    we end up making a dozen differnet enable redis requests and inevitably a lot of those fail because there are so many happening simultaneously (assumption). It's okay to just enable it and leave it on, rather than turning it on and off.
    
    ---------
    
    Co-authored-by: Ryan Wagner <ryan.wagner@pantheon.io>
    Co-authored-by: Chris Reynolds <chris@jazzsequence.com>

---
+++ tests/behat/features/bootstrap/WpRedisFeatureContext.php
@@ -0,0 +1,35 @@
+<?php
+// features/bootstrap/WPRedisFeatureContext.php
+
+namespace behat\features\bootstrap;
+
+use Behat\Behat\Context\Context;
+
+class WpRedisFeatureContext implements Context
+{
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Waits a certain number of seconds.
+     *
+     * @param int $seconds
+     *   How long to wait.
+     *
+     * @When I wait :seconds second(s)
+     */
+    public function wait($seconds)
+    {
+        sleep($seconds);
+    }
+
+}
