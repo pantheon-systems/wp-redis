e228c87fb16cc7ff1ecb6b34c5ab46be7c96f450
Chris Reynolds <chris.reynolds@pantheon.io>
2023-06-23 10:58:58 -0600

    [CMSP-459] bugfix: re-add missing else (#437)
    * re-add missing else
    
    * use the already-defined value of $port instead of hard-coding
    
    * remove ternary in favor of elseif

--- object-cache.php
+++ object-cache.php
@@ -1277,6 +1277,8 @@ class WP_Object_Cache {
 			// port must be null or socket won't connect.
 			unset( $redis_server['port'] );
 			$port = null;
+		} elseif ( ! empty( $redis_server['port'] ) ) { // tcp connection.
+			$port = $redis_server['port'];
 		}
 
 		$defaults = [
