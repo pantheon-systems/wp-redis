e84fb13e405138bbf25b2d2eb640e8222fe26509
Chris Reynolds <chris@jazzsequence.com>
2023-10-12 15:26:18 -0600

    check if the database exists before running redis-enabled tests

--- bin/phpunit-redis-test.sh
+++ bin/phpunit-redis-test.sh
@@ -4,7 +4,25 @@ set -e
 
 DIRNAME=$(dirname "$0")
 
-bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest
+WITHDB="true"
+
+function database_exists {
+  # Usage: database_exists <database_name> <user> <password> <host>
+  local dbname=$1
+  local dbuser=$2
+  local dbpass=$3
+  local dbhost=$4
+  echo "SHOW DATABASES LIKE '${dbname}';" | mysql -u"${dbuser}" -p"${dbpass}" -h"${dbhost}" 2>/dev/null | grep "${dbname}" > /dev/null
+  return $?
+}
+
+# Check if the database exists
+if database_exists wordpress_test root root 127.0.0.1; then
+  echo "Database wordpress_test already exists, we won't install a new one..."
+  WITHDB=""
+fi
+
+bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest "$WITHDB"
 echo "Running WP Redis PHPUnit Tests on Single Site"
 WP_REDIS_USE_CACHE_GROUPS=1 composer phpunit
 rm -rf "$WP_TESTS_DIR" "$WP_CORE_DIR"
